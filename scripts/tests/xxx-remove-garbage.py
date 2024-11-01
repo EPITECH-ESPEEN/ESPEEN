d799c51c0f0830fcdafc7d544c0a62aebdf4c242
Tech0ne <clement.piasco@epitech.eu>
2024-11-01 20:45:03 +0100

    [+] Added remove_garbage to tests

---
+++ scripts/tests/xxx-remove-garbage.py
@@ -0,0 +1,18 @@
+from requests import Session
+
+user = {
+    "username": "allgooduser",
+    "password": "Th1s1s4L*ng4ndVal1dP4ssw0rd!",
+}
+
+def run():
+    output = ""
+    s = Session()
+
+    token = s.post("http://localhost:8080/api/login", json=user).json.get("access_token")
+    s.headers.update({"Autorization": "Bearer " + token})
+
+    # cookies should be all good
+    output += s.delete("/user").text + '\n'
+
+    return (True, "=== All good ===")
